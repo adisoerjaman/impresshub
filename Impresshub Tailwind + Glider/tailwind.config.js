69298a2730d7a20683b2477182e0f85e61f85d01
Adi Soerjaman <56381986+adisoerjaman@users.noreply.github.com>
2022-05-23 12:49:47 +0200

    Add files via upload

---
+++ Impresshub Tailwind + Glider/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'alumni': '#7655AC',
+        'blog': '#e671f5',
+        'festival': '#f5cf50',
+        'game': '#75e296',
+        'podcast': '#3333e3',
+      },
+      fontFamily: {
+        'barlow': ["'Barlow'", 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
